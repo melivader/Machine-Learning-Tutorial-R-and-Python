46fbe473ee4a41a458f190912e9b80e934f56500
Phuwit.n <jabokmax@outlook.com>
2017-03-01 11:45:32 +0700

    Encoding categorical data.

--- 1.data_preprocessing/data_preprocessing.r
+++ 1.data_preprocessing/data_preprocessing.r
@@ -13,3 +13,14 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(
                           x, na.rm = TRUE)),
                         dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3)
+                         )
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('Yes', 'No'),
+                         labels = c(1, 0)
+                         )
