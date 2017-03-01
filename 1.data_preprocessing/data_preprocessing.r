b599504ad94f66f3b687d08b4a875d55ff7242ad
Phuwit.n <jabokmax@outlook.com>
2017-03-01 10:16:39 +0700

    Take care of missing data.

--- 1.data_preprocessing/data_preprocessing.r
+++ 1.data_preprocessing/data_preprocessing.r
@@ -2,3 +2,14 @@
 
 # Importing the Dataset
 dataset = read.csv('./Data.csv')
+
+# Take care of Missing data (n/a | nan)
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(
+                       x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(
+                          x, na.rm = TRUE)),
+                        dataset$Salary)
