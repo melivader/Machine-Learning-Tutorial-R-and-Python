435462d2112d84040d8a163ddf7523079f61fffa
Phuwit.n <jabokmax@outlook.com>
2017-03-02 20:25:51 +0700

    Feature Scaling.

--- 1.data_preprocessing/data_preprocessing.r
+++ 1.data_preprocessing/data_preprocessing.r
@@ -33,4 +33,6 @@ split = sample.split(dataset$Purchased,
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-
+# Feature Scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
