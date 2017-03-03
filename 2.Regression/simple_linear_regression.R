ffaff3fc0cfe29081fadac058478cbee7dc82801
Phuwit.n <jabokmax@outlook.com>
2017-03-03 23:09:54 +0700

    Fitting Simple Linear Regression to the Training set.

---
+++ 2.Regression/simple_linear_regression.R
@@ -0,0 +1,22 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Simple Linear Regression to the Training set
+
+
+# Predicting the Test set results
+
+
+# Visualising the Training set results
+
+# Visualising the Test set results
