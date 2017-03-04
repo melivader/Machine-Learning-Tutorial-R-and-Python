0da48a425b5194397c0debf77a70ac6591c0975d
Phuwit.n <jabokmax@outlook.com>
2017-03-04 23:04:22 +0700

    Fitting Simple Regression

--- 2.Regression/simple_linear_regression.R
+++ 2.Regression/simple_linear_regression.R
@@ -5,14 +5,19 @@ dataset = read.csv('Salary_Data.csv')
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
 # Fitting Simple Linear Regression to the Training set
-
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
 
 # Predicting the Test set results
 
